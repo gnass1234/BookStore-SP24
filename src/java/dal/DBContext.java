01fe828482c72ae9465de2b6b58c2ea6d891e49c
MSI\PC <sangdqhe176316@fpt.edu.vn>
2024-03-09 09:54:34 +0700

    init productDAO and categoryDAO

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+/**
+ *
+ * @author PC
+ */
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+    protected Connection connection;
+    protected PreparedStatement statement;
+    protected ResultSet resultSet;
+
+    /**
+     * get an connection
+     *
+     * @return connection or null
+     * @throws ClassNotFoundException
+     */
+    public Connection getConnection() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://MSI\\\\DOSANG:1433;databaseName=4USER_PRJ301";
+            String user = "sa";
+            String password = "123456";
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Error " + e.getMessage() + " at DBContext");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        DBContext test = new DBContext();
+        test.connection = test.getConnection();
+        System.out.println(test.connection);
+    }
+}
